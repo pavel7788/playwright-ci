bc78b81ad26ffdac752c7b3748326a44f3d97043
Pavlo Iaroshenko <p.iaroshenko@gmail.com>
2025-10-24 18:19:25 +0300

    git commit test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,6 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
+  // git test
   await expect(page).toHaveTitle(/Playwright/);
 });
 
